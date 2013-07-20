f6cffe83196851c84325269da83787d0769b939a
OpenShift guest <philgoebel@gmail.com>
2013-07-20 06:35:59 -0400

    Initial commit

---
+++ recovery/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recovery::Application.config.secret_token = '26341a6eac92fd6c4974588d285fdf7e36e1a52e79cc0275f8791c51ddb8d7c25dd6949ca8e69faf3910d0c205d8d5f8313017b4597a1f9a54e4f032a38afd79'
